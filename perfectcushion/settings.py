5ebc75a828ea590c38a372900adcccfd03864fd6
Jennifer Wen <jenniferwen84@gmail.com>
2019-05-05 21:12:01 +0800

    Update settings.py

--- perfectcushion/settings.py
+++ perfectcushion/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6ri9zvqwu93wtw7m_!2&b@9%jt!yy-hqe)ofm)@jbeb^rcl313'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
